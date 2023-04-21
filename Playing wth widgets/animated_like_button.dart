e4690ad973e59eadc32c60ff915cb77f45fe44c4
Punnya Prasoon Gaur <gaurpunnya.02@gmail.com>
2023-04-21 13:29:21 +0530

    Add files via upload

---
+++ Playing wth widgets/animated_like_button.dart
@@ -0,0 +1,52 @@
+import "package:flutter/material.dart";
+import 'package:like_button/like_button.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: AnimatedLikeButton(),
+    darkTheme: ThemeData(brightness: Brightness.dark),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class AnimatedLikeButton extends StatefulWidget {
+  const AnimatedLikeButton({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedLikeButton> createState() => _AnimatedLikeButtonState();
+}
+
+// void func() {
+//   if (count == 0) {
+//     count++;
+//     // } else {
+//     //   count = count;
+//   }
+// }
+
+class _AnimatedLikeButtonState extends State<AnimatedLikeButton> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'L I K E B U T T O N',
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+            child: LikeButton(
+          likeCount: 0,
+          size: 70,
+          circleSize: 300,
+          bubblesSize: 300,
+          likeCountAnimationType: LikeCountAnimationType.part,
+          animationDuration: Duration(milliseconds: 2000),
+          countPostion: CountPostion.bottom,
+        )),
+      ),
+    );
+  }
+}
